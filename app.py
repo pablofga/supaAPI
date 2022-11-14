8e9cd217f11ded9621c5ad3d7ec2cd919dfa18bf
pablo_fga <pablo.fga@gmail.com>
2022-11-14 16:33:09 +0100

    20221114

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ supabase: Client = create_client(url, key)
 
 @app.route('/')
 def main_page():
-    return "<html><head></head><body>@pfga A RESTful API in Flask using SQLAlchemy. For more info on usage, go to <a href>https://github.com/mgreenw/flask-restapi-example</a>.</body></html>"
+    return "<html><head></head><body>@PRUEBA pfga A RESTful API in Flask using SQLAlchemy. For more info on usage, go to <a href>https://github.com/mgreenw/flask-restapi-example</a>.</body></html>"
 
 # Mostrar Doctor
 @app.route('/api/v1/posts/<id2>', methods=['GET'])
 def show_post(id2):
     try:
         post = supabase.table("posts").select("*").eq("id",id2).execute()
-        return jsonify(post.serialize),200
+        return jsonify(post.data),200
     except:
         return not_found("Post does not exis1")
 
